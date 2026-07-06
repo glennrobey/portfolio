9b99da1f7e7708f9dc05fd09d9af39c3a4d8ec40
Glenn Robey <glennrobey95@gmail.com>
2026-07-06 16:06:13 -0500

    Done! More Minor Tweaking

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 
 import Home from "./pages/home";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
-
-import tux from "./assets/tux.jpg";
 
 export default function App() {
   return (
