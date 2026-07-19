556bc95fe15ea6835f3661503b917f8771c277e0
Glenn Robey <glennrobey95@gmail.com>
2026-07-19 10:22:45 -0500

    update portfolio

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,5 @@
 import crypt0 from "../assets/crypt0.png";
+import cppcalc from "../assets/cppcalc.jpg";
 
 export default function Projects() {
   return (
@@ -9,7 +10,7 @@ export default function Projects() {
           glenn@portfolio:~$ ls projects
         </p>
 
-        {/* Project 1 - Portfolio */}
+        {/* Portfolio */}
         <div className="mb-8">
           <h2 className="text-2xl font-semibold text-green-400">Portfolio</h2>
 
@@ -23,7 +24,7 @@ export default function Projects() {
 
         <hr className="border-green-500/20 my-6" />
 
-        {/* Project 2 - crypt0.current_ */}
+        {/* Project 1 - crypt0.current_ */}
         <div className="mb-8">
           <h2 className="text-2xl font-semibold text-green-400">
             crypt0.current_
@@ -40,6 +41,27 @@ export default function Projects() {
           <p className="text-gray-300 mt-4 leading-relaxed">
             Cryptocurrency and fiat currency converter built with React, Vite,
             PostgreSQL, Node.js, authentication, and a community forum.
+            <span className="animate-blink text-green-400">_</span>
+          </p>
+        </div>
+
+        <hr className="border-green-500/20 my-6" />
+
+        {/* Project 2 - C++Calc */}
+        <div className="mb-8">
+          <h2 className="text-2xl font-semibold text-green-400">C++Calc</h2>
+
+          <div className="mt-4 border border-green-500/20 rounded-lg overflow-hidden">
+            <img
+              src={cppcalc}
+              alt="crypt0 preview"
+              className="w-full transition duration-300 transform hover:scale-110 hover:shadow-2xl hover:shadow-green-500/30"
+            />
+          </div>
+
+          <p className="text-gray-300 mt-4 leading-relaxed">
+            Scientific calculator built with C++, React, Drogon, Docker, custom
+            tokenization, expression parsing, and REST API communication.
             <span className="animate-blink text-green-400">_</span>
           </p>
         </div>
